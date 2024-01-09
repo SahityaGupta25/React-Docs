1d8b8988fb0dc34a87c6722e97ff6b8b37bc481e
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-01-09 08:23:22 +0530

    updated

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,8 +4,8 @@ function Background() {
   return (
     
     <>
-    <div className="fixed w-full h-screen z-[2]">
-    <div className="w-full py-10 flex justify-center font-semibold text-zinc-500 text-xl">Documents</div>
+    <div className="fixed w-full h-screen z-[2]"> 
+    <div className="w-full py-10 flex justify-center font-semibold text-zinc-500 text-xl">Documents</div> //^Used as Navbar
     <h1 className="absolute top-1/2 left-1/2 font-semibold text-6xl leading-none tracking-tighter text-zinc-300">Docs</h1>
 
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { FaFileAlt } from "react-icons/fa";
+import { MdDownloadForOffline } from "react-icons/md";
 
 function Card() {
   return (
-    <div className="w-80 h-72 rounded-xl bg-zinc-300">
+    <div className="overflow-hidden relative w-80 h-72 rounded-[50px] bg-zinc-600 text-white px-5 py-10">
         <FaFileAlt />
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat nam repellat velit consequuntur saepe a eligendi!</p>
+        <div className='footer absolute  left-0 bottom-0 w-full'>
+            <div className="flex items-center py-3 justify-between px-6">
+
+            <h5>0.6MB</h5>
+            <span className='w-7 h-7'>
+
+            <MdDownloadForOffline size="1.2em"  />
+            </span>
+            </div>
+            <div className="tag w-full bg-sky-300 py-3">
+                <h3>Download Now</h3>
+            </div>
+        </div>
     </div>
   )
 }
