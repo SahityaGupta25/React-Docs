a2e48332fce934377c7a284143381326491d389f
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-01-09 13:57:04 +0530

    comp

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { FaFileAlt } from "react-icons/fa";
+import { FaRegFileAlt } from "react-icons/fa";
 import { MdDownloadForOffline } from "react-icons/md";
 
-function Card() {
+function Card({data}) {
   return (
-    <div className="overflow-hidden relative w-80 h-72 rounded-[50px] bg-zinc-600 text-white px-5 py-10">
-        <FaFileAlt />
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat nam repellat velit consequuntur saepe a eligendi!</p>
+    <div className="overflow-hidden relative w-80 h-72 rounded-[46px] bg-zinc-600 text-white px-5 py-10">
+        <FaRegFileAlt className='h-6'/>
+        <p className='pt-4 leading-none'>{data.description}</p>
         <div className='footer absolute  left-0 bottom-0 w-full'>
             <div className="flex items-center py-3 justify-between px-6">
 
@@ -16,7 +16,7 @@ function Card() {
             <MdDownloadForOffline size="1.2em"  />
             </span>
             </div>
-            <div className="tag w-full bg-sky-300 py-3">
+            <div className="tag w-full bg-green-600 font-semibold text-s py-3 flex justify-center items-center">
                 <h3>Download Now</h3>
             </div>
         </div>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
 
 function Foreground() {
+    const data =[
+        {
+            description:"Grand Theft Auto V for PC features a range of major visual and technical upgrades to make Los Santos and Blaine County more immersive than ever.",
+            filesize:"98.66GB",
+            close:false,
+            tagdetails:{isOpen:true,tagTitle:"Download Now",tagColor:"green"}
+        }
+    ]
+    useState()
+
   return (
     <>
      <div className="fixed z-[3] left-0 top-0 w-full h-full ">
-        <Card/>
+        {data.map((item,index)=>(
+            <Card data={item}/>
+        ))}
+       
         
      </div>
 
